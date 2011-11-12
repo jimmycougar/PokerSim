324a088e1732048b07a9fa75536a1b2947ce61c7
Estelle <ehkim3@gmail.com>
2011-11-11 23:18:21 -0500

    Add Card Class

---
+++ Card.cpp
@@ -0,0 +1,19 @@
+#include "Card.h"
+
+Card::Card(NUMBER inNum, SUIT inSuit) :
+	num(inNum),
+	suit(inSuit)
+{
+	
+}
+
+Card::NUMBER Card::GetNumber() 
+{
+	return num;
+}
+
+Card::SUIT Card::GetSuit() 
+{
+	return suit;
+}
+
